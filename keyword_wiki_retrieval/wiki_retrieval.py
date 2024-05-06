70fe490b9dd530303d47a62e221f7f8d1f6c5535
Zhengpu Hu <zhhu3786@uni.sydney.edu.au>
2024-05-06 14:30:23 +1000

    Initialise basics with wiki retrieval

---
+++ keyword_wiki_retrieval/wiki_retrieval.py
@@ -0,0 +1,21 @@
+import pywikibot
+
+
+# Function to retrieve information from Wikipedia
+def get_wikipedia_info(query):
+    try:
+        # Set language of Wikipedia you want to search
+        site = pywikibot.Site("en", "wikipedia")
+
+        # Search for the query on Wikipedia
+        page = pywikibot.Page(site, query)
+
+        # Retrieve title, URL, and summary of the Wikipedia page
+        title = page.title()
+        url = page.full_url()
+        summary = page.text[:500]  # Extracting first 500 characters as summary
+
+        return title, url, summary
+    except pywikibot.exceptions.PageRelatedError as e:
+        # If the page doesn't exist or other related errors occur, handle it
+        return f"Error: {e}", None, None
