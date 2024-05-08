30f93dc586cd6d1a9c8f5584480099eca58380c2
Hansxsourse <xsourse.cc@gmail.com>
2024-05-08 15:12:13 +0000

    summarization part

---
+++ llm_eval_data_prepare.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data_df = pd.read_csv("data/news_summary/news_summary.csv", encoding="latin1")
+# drop the row containing NaN
+data_df = data_df.dropna()
+
+# sample 50 from the dataset
+data_df = data_df.sample(50)
+
+# save the sampled data
+data_df.to_csv("data/news_summary/news_summary_sampled.csv", index=False, encoding="utf-8")
