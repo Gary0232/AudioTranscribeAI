4fb3e0ae0ae8d06f10ac6d77d4e45a81e1c898a0
Gary Du <gudu9182@uni.sydney.edu.au>
2024-05-10 06:20:38 +1000

    add qa UI

--- app.py
+++ app.py
@@ -1,23 +1,103 @@
+from flask_sqlalchemy import SQLAlchemy
+import os
+import mimetypes
+import hashlib
+import moviepy.editor as mp
 from flask import Flask, request, jsonify
-
 from keyword_wiki_retrieval.wiki_retrieval import get_wikipedia_info
 from model import audio_recognition
 from flask_cors import CORS
+from sqlalchemy import String, Column, Integer, Float, Text, ForeignKey, DateTime, JSON, ARRAY
 
 app = Flask(__name__)
 CORS(app)
 
+FILE_UPLOAD_FOLDER = 'uploads'
+
+base_bath = os.path.dirname(__file__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_bath, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class ProcessedFile(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(255), nullable=False)
+    file_path = db.Column(Text, nullable=False)
+    audio_recognition_result = db.Column(JSON)
+    text_summarization_result = db.Column(Text)
+
+
+# create the table if it doesn't exist
+with app.app_context():
+    db.create_all()
+
+# create the folder if it doesn't exist
+if not os.path.exists(FILE_UPLOAD_FOLDER):
+    os.makedirs(FILE_UPLOAD_FOLDER)
+
 
 @app.route("/file_upload", methods=['POST'])
 def api_media_recognition():
-    # get the file from the request
-    file = request.files['file']
+    # Get the file from the request
+    file = request.files.get('file')
     if not file:
-        return jsonify({"status": "error", "message": "No file provided"})
-    # process the file
-    result = audio_recognition(file.filename)
+        return jsonify({"status": "error", "message": "No file provided"}), 400
+
+    # Check the file type using mimetypes
+    mime_type, encoding = mimetypes.guess_type(file.filename)
+    allowed_mime_types = ['audio/wav', 'audio/mpeg', 'audio/ogg', 'audio/x-flac', 'video/mp4']
+    if mime_type not in allowed_mime_types:
+        return jsonify({"status": "error", "message": "Invalid file type"}), 400
+
+    # Generate a hash value as the filename
+    hash_value = hashlib.md5(file.read()).hexdigest()
+    file.seek(0)
+    audio_filename = f"{hash_value}.mp3"
+    audio_file = ProcessedFile.query.filter_by(name=audio_filename).first()
+    if audio_file:
+        # if the file has been processed before, return the result directly
+        return jsonify({
+            "status": "success",
+            "hash": hash_value,
+            "recognition_result": audio_file.audio_recognition_result
+        })
+
+    # else save the file and process it
+    audio_filepath = os.path.join(FILE_UPLOAD_FOLDER, audio_filename)
+    temp_file_path = os.path.join(FILE_UPLOAD_FOLDER, f"{hash_value}_temp")
+    file.save(temp_file_path)
+    # Extract audio if the file is an MP4 video
+    if mime_type == 'video/mp4':
+        video = mp.VideoFileClip(temp_file_path)
+        audio = video.audio
+        audio.write_audiofile(audio_filepath, codec='libmp3lame')
+        audio.close()
+        video.close()
+
+    else:
+        audio_clip = mp.AudioFileClip(temp_file_path)
+        audio_clip.write_audiofile(audio_filepath, codec='libmp3lame')
+        audio_clip.close()
+    # Remove the temporary video file
+    os.remove(temp_file_path)
+
+    # Process the audio file
+    result = audio_recognition(audio_filepath)
+
+    # Store file info in the database
+    processed_file = ProcessedFile(
+        name=audio_filename,
+        file_path=audio_filepath,
+        audio_recognition_result=result
+    )
+    db.session.add(processed_file)
+    db.session.commit()
+
     return jsonify({
         "status": "success",
+        "hash": hash_value,
         "recognition_result": result
     })
 
@@ -35,6 +115,7 @@ def api_wikipedia():
         "wikipedia_result": keyword
     })
 
+
 @app.route("/summarize", methods=['POST'])
 def api_text_summarization():
     # get the text from the request
@@ -49,6 +130,19 @@ def api_text_summarization():
     })
 
 
+@app.route("/qa", methods=['POST'])
+def api_qa():
+    question = request.json.get('question')
+    file_hash = request.json.get('hash')
+    if not question or not file_hash:
+        return jsonify({"status": "error", "message": "No question or hash provided"})
+    # process the question
+    # answer = qa(question, file_hash)
+    return jsonify({
+        "status": "success",
+        "answer": "this is text answer"
+    })
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- frontend/AudioTranscribeAI/src/components/Main.vue
+++ frontend/AudioTranscribeAI/src/components/Main.vue
@@ -3,26 +3,44 @@ import {ref, watch} from "vue";
 import axios from 'axios';
 import CompromiseTextBox from "@/components/CompromiseTextBox.vue";
 import {debounce} from "lodash";
+import {da} from "vuetify/locale";
 
 
 const apiUrl = 'http://localhost:5000';
 // variable to store file upload
-const files = ref(null);
+
 
 // current step
 const step = ref(0);
 
 
+// Step 1
+const file_recognition_loading = ref(false);
+const fileHash = ref(null);
 const recognitionResult = ref("");
 const recognitionTokenizedResult = ref([]);
-const wikipediaResult = ref(null);
-const summarizationResult = ref(null);
-const selectedWord = ref(null);
+const files = ref(null);
+const selected_language = ref({state: 'English', abbr: 'en'})
+const available_language = [
+  {state: 'English', abbr: 'en'},
+  {state: 'Japanese', abbr: 'ja'},
+  {state: 'French', abbr: 'fr'},
+  {state: 'German', abbr: 'de'},
+  {state: 'Spanish', abbr: 'es'},
+  {state: 'Russian', abbr: 'ru'},
+]
 
+// step 2
 // on watch the selected word
+const selectedWord = ref(null);
+const wikipediaResult = ref(null);
 watch(selectedWord, (newValue) => {
   fetchWikipedia(newValue);
 });
+
+// step 3
+const summarizationResult = ref(null);
+const hideQADrawer = ref(true);
 
 const fetchWikipedia = debounce((word) => {
   if (word) {
@@ -38,10 +56,14 @@ const fetchWikipedia = debounce((word) => {
   }
 }, 1000);
 
+// QA
+const qaList = ref([]);
+
 function submitFile() {
+  file_recognition_loading.value = true;
   const formData = new FormData();
   formData.append('file', files.value);
-
+  formData.append('language', selected_language.value.abbr);
   axios.post(apiUrl + '/file_upload', formData, {
     headers: {
       'Content-Type': 'multipart/form-data'
@@ -51,9 +73,12 @@ function submitFile() {
     if (response.data.status === 'success') {
       recognitionResult.value = response.data.recognition_result.text;
       recognitionTokenizedResult.value = response.data.recognition_result.tokens;
+      fileHash.value = response.data.hash;
     }
   }).catch(error => {
     console.error(error);
+  }).finally(() => {
+    file_recognition_loading.value = false;
   });
 }
 
@@ -67,11 +92,39 @@ function generateSummary() {
   });
 }
 
+const question = ref(null);
+const qaLoading = ref(false);
+function sendQuestion() {
+  const questionData = question.value;
+  if (!questionData) {
+    return;
+  }
+  qaLoading.value = true;
+  axios.post(apiUrl + '/qa', {
+    question: questionData,
+    hash: fileHash.value
+  }).then(response => {
+    qaList.value.push({
+      who: 'user',
+      text: questionData
+    });
+    qaList.value.push({
+      who: 'model',
+      text: response.data.answer
+    });
+  }).catch(error => {
+    console.error(error);
+  }).finally(() => {
+    question.value = null;
+    qaLoading.value = false;
+  });
+}
+
 </script>
 
 <template>
   <v-container fluid class="fill-height ma-0 pa-0">
-    <v-row class="fill-height">
+    <v-row class="fill-height mx-2">
       <v-col cols="4" class="h-100" v-if="step === 0">
         <v-card class="fill-height">
           <v-card-title>
@@ -79,15 +132,32 @@ function generateSummary() {
           </v-card-title>
           <v-card-text>
             <v-file-input
-              accept="audio/*, video/*"
+              accept="audio/*, video/mp4"
               label="Upload the audio/video file here"
               placeholder="No file chosen"
               prepend-icon="mdi-multimedia"
               show-size
               v-model="files"
             ></v-file-input>
+
+
+            <v-select
+              v-model="selected_language"
+              :hint="`${selected_language.state}, ${selected_language.abbr}`"
+              :items="available_language"
+              item-title="state"
+              item-value="abbr"
+              label="Choose the language of the source file"
+              persistent-hint
+              return-object
+              prepend-icon="mdi-translate"
+            ></v-select>
+
             <v-card-actions>
-              <v-btn @click="submitFile" variant="flat" color="blue" :disabled="!files" block>Submit</v-btn>
+              <v-btn @click="submitFile" variant="flat" color="blue"
+                     :disabled="!files || !selected_language || file_recognition_loading" block>
+                Submit
+              </v-btn>
             </v-card-actions>
           </v-card-text>
         </v-card>
@@ -110,6 +180,9 @@ function generateSummary() {
             <v-btn @click="step = 0" v-if="step === 1" variant="flat" color="blue">
               Back to File Upload
             </v-btn>
+            <v-btn color="purple" v-if="step === 1" variant="flat" @click="generateSummary">
+              Generate Summary
+            </v-btn>
           </v-card-actions>
         </v-card>
         <v-card class="fill-height" v-else>
@@ -129,7 +202,7 @@ function generateSummary() {
           <v-card-text class="flex-grow-1" v-if="selectedWord == null">
             Select a word to fetch Wikipedia information
           </v-card-text>
-          <v-card-text class="flex-grow-1" v-else-if="wikipediaResult !== null" >
+          <v-card-text class="flex-grow-1" v-else-if="wikipediaResult !== null">
             {{ wikipediaResult }}
           </v-card-text>
           <v-card-text v-else class="flex-grow-1 d-flex flex-column justify-center align-center">
@@ -137,17 +210,15 @@ function generateSummary() {
               indeterminate
             ></v-progress-circular>
           </v-card-text>
-          <v-card-actions>
-            <v-btn block color="purple" variant="flat" @click="generateSummary">
-              Generate Summary
-            </v-btn>
-          </v-card-actions>
         </v-card>
       </v-col>
       <v-col cols="4" v-if="summarizationResult" class="h-100">
         <v-card class="fill-height d-flex flex-column">
-          <v-card-title>
+          <v-card-title class="d-flex justify-space-between">
             Text Summarization
+            <v-btn @click="summarizationResult = null" icon variant="plain">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
           </v-card-title>
           <v-card-text style="overflow-y: auto;">
             {{ recognitionResult }}
@@ -155,6 +226,36 @@ function generateSummary() {
         </v-card>
       </v-col>
     </v-row>
+    <v-navigation-drawer
+      width="400"
+      location="right"
+      permanent
+      :rail="hideQADrawer"
+      v-if="step === 1"
+    >
+      <v-btn @click="hideQADrawer = !hideQADrawer" variant="text" icon v-if="hideQADrawer">
+        <v-icon>{{ hideQADrawer ? 'mdi-chat-question' : 'mdi-chevron-right' }}</v-icon>
+      </v-btn>
+      <v-list color="transparent" v-if="!hideQADrawer">
+        <v-list-item v-for="data in qaList" :key="data.text"
+                     class="text-caption"
+                     :prepend-icon="data.who === 'model'? 'mdi-view-dashboard' : ''"
+                     :append-icon="data.who === 'model'? '' : 'mdi-account'">
+          {{ data.text }}
+        </v-list-item>
+      </v-list>
+      <template v-slot:append>
+        <v-textarea :loading="qaLoading" rows="4" v-model="question" placeholder="Ask a question here..."
+                    no-resize hide-details rounded="0" v-if="!hideQADrawer"></v-textarea>
+        <v-btn rounded="0" v-if="!hideQADrawer" @click="sendQuestion" :disabled="qaLoading" color="primary">
+          Send
+        </v-btn>
+        <v-btn rounded="0" @click="hideQADrawer = !hideQADrawer" v-if="!hideQADrawer">
+          Hide
+        </v-btn>
+
+      </template>
+    </v-navigation-drawer>
   </v-container>
 </template>
 
